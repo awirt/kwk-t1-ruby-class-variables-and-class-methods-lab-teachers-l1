f66ce1b62df236d0a07ce9ff3cdfd413a17789e9
Andrew Wirt <awjwirt@gmail.com>
2018-07-02 04:34:11 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+class Song
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count += 1
+    @@genres << @genre
+    @@artists << @artist
+  end
+  
+  attr_accessor :name, :artist, :genre
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |i|
+      
+    end
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.artist_count
+  end
+  
+end
